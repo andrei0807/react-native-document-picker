7f5651df361603d5c6820187ca5e04da1ee7ab35
sky <sky@skys-iMac.local>
2018-02-03 19:52:18 +0800

    first commit

---
+++ ios/RNDocumentPicker/RNDocumentPicker.m
@@ -0,0 +1,166 @@
+#import "RNDocumentPicker.h"
+
+#import <MobileCoreServices/MobileCoreServices.h>
+
+#if __has_include(<React/RCTConvert.h>)
+#import <React/RCTConvert.h>
+#import <React/RCTBridge.h>
+#else // back compatibility for RN version < 0.40
+#import "RCTConvert.h"
+#import "RCTBridge.h"
+#endif
+
+#define IDIOM    UI_USER_INTERFACE_IDIOM()
+#define IPAD     UIUserInterfaceIdiomPad
+
+@interface RNDocumentPicker () <UINavigationControllerDelegate,UIImagePickerControllerDelegate> //<UIDocumentMenuDelegate,UIDocumentPickerDelegate>
+@end
+
+
+@implementation RNDocumentPicker {
+    NSMutableArray *composeViews;
+    NSMutableArray *composeCallbacks;
+}
+
+@synthesize bridge = _bridge;
+
+- (instancetype)init
+{
+    if ((self = [super init])) {
+        composeCallbacks = [[NSMutableArray alloc] init];
+        composeViews = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_MODULE()
+RCT_EXPORT_METHOD(show:(NSDictionary *)options
+                  callback:(RCTResponseSenderBlock)callback) {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
+    imagePicker.delegate = self;
+    imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    imagePicker.mediaTypes = [[NSArray alloc] initWithObjects:(NSString *)kUTTypeMovie,      nil];
+    
+    [composeCallbacks addObject:callback];
+    
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    while (topController.presentedViewController) {
+        topController = topController.presentedViewController;
+    }
+    [topController presentViewController:imagePicker animated:YES completion:nil];
+    //[self presentViewController:imagePicker animated: YES completion: nil];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    NSString *mediaType = [info objectForKey: UIImagePickerControllerMediaType];
+    NSString *moviePath;
+    NSMutableDictionary* result = [NSMutableDictionary dictionary];
+    if (CFStringCompare ((__bridge CFStringRef) mediaType, kUTTypeMovie, 0) == kCFCompareEqualTo) {
+        NSURL *videoUrl=(NSURL*)[info objectForKey:UIImagePickerControllerMediaURL];
+        moviePath = [videoUrl path];
+        
+        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
+            //    UISaveVideoAtPathToSavedPhotosAlbum (moviePath, nil, nil, nil);
+        }
+        [result setValue:moviePath forKey:@"uri"];
+        [result setValue:@"" forKey:@"fileName"];
+        [result setValue:@"" forKey:@"fileSize"];
+    }else{
+        
+    }
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    RCTResponseSenderBlock callback = [composeCallbacks lastObject];
+    [composeCallbacks removeLastObject];
+    
+    callback(@[[NSNull null], result]);
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+/*
+RCT_EXPORT_METHOD(show1:(NSDictionary *)options
+                  callback:(RCTResponseSenderBlock)callback) {
+
+    NSArray *allowedUTIs = [RCTConvert NSArray:options[@"filetype"]];
+    UIDocumentMenuViewController *documentPicker = [[UIDocumentMenuViewController alloc] initWithDocumentTypes:(NSArray *)allowedUTIs inMode:UIDocumentPickerModeImport];
+
+    [composeCallbacks addObject:callback];
+
+
+    documentPicker.delegate = self;
+    documentPicker.modalPresentationStyle = UIModalPresentationFormSheet;
+
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
+    while (rootViewController.modalViewController) {
+        rootViewController = rootViewController.modalViewController;
+    }
+
+    if ( IDIOM == IPAD ) {
+        NSNumber *top = [RCTConvert NSNumber:options[@"top"]];
+        NSNumber *left = [RCTConvert NSNumber:options[@"left"]];
+        [documentPicker.popoverPresentationController setSourceRect: CGRectMake([left floatValue], [top floatValue], 0, 0)];
+        [documentPicker.popoverPresentationController setSourceView: rootViewController.view];
+    }
+
+    [rootViewController presentViewController:documentPicker animated:YES completion:nil];
+}
+
+
+- (void)documentMenu:(UIDocumentMenuViewController *)documentMenu didPickDocumentPicker:(UIDocumentPickerViewController *)documentPicker {
+    documentPicker.delegate = self;
+    documentPicker.modalPresentationStyle = UIModalPresentationFormSheet;
+
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
+    
+    while (rootViewController.modalViewController) {
+        rootViewController = rootViewController.modalViewController;
+    }
+    if ( IDIOM == IPAD ) {
+        [documentPicker.popoverPresentationController setSourceRect: CGRectMake(rootViewController.view.frame.size.width/2, rootViewController.view.frame.size.height - rootViewController.view.frame.size.height / 6, 0, 0)];
+        [documentPicker.popoverPresentationController setSourceView: rootViewController.view];
+    }
+
+    [rootViewController presentViewController:documentPicker animated:YES completion:nil];
+}
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url {
+    if (controller.documentPickerMode == UIDocumentPickerModeImport) {
+        RCTResponseSenderBlock callback = [composeCallbacks lastObject];
+        [composeCallbacks removeLastObject];
+
+        [url startAccessingSecurityScopedResource];
+
+        NSFileCoordinator *coordinator = [[NSFileCoordinator alloc] init];
+        __block NSError *error;
+
+        [coordinator coordinateReadingItemAtURL:url options:NSFileCoordinatorReadingResolvesSymbolicLink error:&error byAccessor:^(NSURL *newURL) {
+            NSMutableDictionary* result = [NSMutableDictionary dictionary];
+
+            [result setValue:newURL.absoluteString forKey:@"uri"];
+            [result setValue:[newURL lastPathComponent] forKey:@"fileName"];
+
+            NSError *attributesError = nil;
+            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:newURL.path error:&attributesError];
+            if(!attributesError) {
+                [result setValue:[fileAttributes objectForKey:NSFileSize] forKey:@"fileSize"];
+            } else {
+                NSLog(@"%@", attributesError);
+            }
+
+            callback(@[[NSNull null], result]);
+        }];
+
+        [url stopAccessingSecurityScopedResource];
+    }
+}
+*/
+@end
